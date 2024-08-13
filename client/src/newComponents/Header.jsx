2559281137a46736318bb876e815ba58b436ebb6
matan <matan@iammaran.com>
2024-08-13 16:23:58 +0300

    reset on logo

--- client/src/newComponents/Header.jsx
+++ client/src/newComponents/Header.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 function Header(props) {
     return (
         <div className={"flex flex-row h-24 bg-primary items-center justify-center"}>
-            <img src={"qsplit_logo.svg"}/>
+            <img src={"qsplit_logo.svg"} onClick={() => fetch('/api/reset')}
+            />
         </div>
     );
 }
