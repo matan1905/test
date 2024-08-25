c9fa7920058da39fa3e66811e931416bbae17d5b
matan <matan@iammaran.com>
2024-08-25 22:03:33 +0300

    change

--- client/src/newComponents/HotelDetails.jsx
+++ client/src/newComponents/HotelDetails.jsx
@@ -6,9 +6,9 @@ function FlightDetails(props) {
             className={"flex flex-row items-center justify-between w-full max-w-3xl mt-4 border-opacity-50 font-light border border-primary p-4 rounded-2xl"}>
             <div className={"flex flex-row items-center space-x-4"}>
                 <img src={"hotel_room.png"} className={"rounded-2xl"} alt={"hotel image"} width={100} height={100}/>
-                <div className={"flex flex-col"}>
+                <div className={"flex flex-col "}>
                     <span className={" text-2xl"}>Your Hotel</span>
-                    <div className={"text-sm"}>
+                    <div className={"text-sm flex-col flex lg:flex-row"}>
                         <span>Two Standard Rooms</span>
                         <span>・</span>
                         <span>Four Adults</span>
@@ -19,7 +19,9 @@ function FlightDetails(props) {
             </div>
 
 
-            <img src={"group_of_people.svg"} alt={"group of people"}/>
+            <img
+                className={"hidden sm:block"}
+                src={"group_of_people.svg"} alt={"group of people"}/>
         </div>
     );
 }
